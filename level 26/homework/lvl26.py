c4351881812308d1c6b6d9b9e212fbb0df4767aa
ilia543 <mrmainxpx@gmail.com>
2024-07-13 20:54:43 +0400

    lvl26

---
+++ level 26/homework/lvl26.py
@@ -0,0 +1,292 @@
+# # 1)
+# for i in range(5):
+#     i = i + 1
+#     print(i)
+
+# # 2)
+# i = 0
+# while i < 5:
+#     i = i + 1
+#     print(i)
+
+# # 3)
+# for i in range(10):
+#     print("hello")
+
+# # 4)
+# i = 0
+# while i < 10:
+#     i = i + 1
+#     print("hello")
+
+# # 5)
+# i = 1
+# for i in range(3):
+#     i = i + 1
+#     print(i)
+
+# # 6)
+# i = 0
+# while i <= 2:
+#     i = i + 1
+#     print(i)
+
+# # 7)
+# for i in range(3):
+#     print("python")
+
+# # 8)
+# i = 0
+# while i < 3:
+#     i = i + 1
+#     print("python")
+
+# # 9)
+# list = [1,2,3]
+# for i in range(1):
+#     print(list)
+
+# # 10)
+# list = [1,2,3]
+# i = 1
+# while i <=1:
+#     i = i + 1
+#     print(list)
+
+# # 11)
+# list = [1,2,3]
+# for i in range(len(list)):
+#     print("hi " + str(list[i]))
+
+# # 12)
+# list = ["1","2","3"]
+# i = 1
+# while i <= 1:
+#     i = i + 1
+#     for k in range(3):
+#         print("hi " + list[k])
+
+# # 13)
+# i = 1
+# for i in range(3):
+#     i = i + 1
+#     print(i)
+
+# # 14)
+# i = 0
+# while i <= 2:
+#     i = i + 1
+#     print(i)
+
+# # 15)
+# i = 4
+# for l in range(3):
+#     i = i - 1
+#     print(i)
+
+# # 16)
+# i = 4
+# while i > 1:
+#     i = i - 1
+#     print(i)
+
+# # 17)
+# alp = ["a","b","c","d"]
+# for i in range(1):
+#     print(alp)
+
+# # 18)
+# alp = ["a","b","c","d"]
+# i = 1
+# while i <= 1:
+#     i = i + 1
+#     print(alp)
+
+# # 19)
+# word = "looping"
+# for i in range(4):
+#     print(word)
+
+# # 20)
+# word = "looping"
+# i = 0
+# while i < 4:
+#     i = i + 1
+#     print(word)
+
+# # 21)
+# list = [1,2,3]
+# for i in range(3):
+#     print(list[i] * 2)
+
+# # 22)
+# list = [1,2,3]
+# i = -1
+# while i <= 1:
+#     i = i + 1
+#     print(list[i] * 2)
+
+# # 23)
+# i = 6
+# for k in range(5):
+#     i = i - 1
+#     print(i)
+
+# # 24)
+# i = 6
+# while i > 1:
+#     i = i - 1
+#     print(i)
+
+# # 25)
+# list = ["apple", "banana", "cherry"]
+# for i in range(len(list)):
+#     print(list[i])
+
+# # 26)
+# list = ["apple", "banana", "cherry"]
+# i = 1
+# while i <= 1:
+#     i = i + 1
+#     for k in range(len(list)):
+#         print(list[k])
+
+# # 27)
+# for i in range(3):
+#     print(i)
+
+# # 28)
+# i = -1
+# while i < 2:
+#     i = i + 1
+#     print(i)
+
+# # 29)
+# list = [1,2,3,4]
+# for i in range(1):
+#     print((len(list) * "loop "))
+
+# # 30)
+# list = [1,2,3,4]
+# i = 0
+# while i < 4:
+#     i = i + 1
+#     print("loop")
+
+# # 31)
+# alp = "abcdchad"
+# for i in range(3):
+#     print(alp[i])
+
+# # 32)
+# alp = "abcdchad"
+# i = -1
+# while i <= 1:
+#     i = i + 1
+#     print(alp[i])
+
+# # 33)
+# list = ["x","y","z"]
+# for i in range(2):
+#     print(list[i])
+
+# # 34)
+# list = ["x","y","z"]
+# i = -1
+# while i <= 0:
+#     i = i + 1
+#     print(list[i])
+
+# # 35)
+# bababui = "hello world"
+# for i in range(2):
+#     print(bababui)
+
+# # 36)
+# bababui = "hello world"
+# i = 10
+# while i <= 11:
+#     i = i + 1
+#     print(bababui)
+
+# # 37)
+# lst = {1,2,3}
+# for i in range(1):
+#     print(lst)
+
+# # 38)
+# lst = {1,2,3}
+# i = 1
+# while i < 2:
+#     i = i + 1
+#     print(lst)
+
+# # 39)
+# list = {"a": 1, "b": 2}
+# for i in range(1):
+#     print(list)
+
+# # 40)
+# list = {"a": 1, "b": 2}
+# i = 1
+# while i <= 1:
+#     i = i + 1
+#     print(list)
+
+# # 41)
+# for i in range(1 , 31):
+#     if i % 10 == 0:
+#         print(i)
+
+# # 42)
+# i = 1
+# while i <= 29:
+#     i = i + 1
+#     if i % 10 == 0:
+#         print(i)
+
+# # 43)
+# for i in range(3):
+#     print(i)
+# print("done")
+
+# # 44)
+# i = 0
+# while i <= 5:
+#     i = i + 1
+#     print(i)
+# print("done")
+
+# # 45)
+# list = [[1, 2], [3, 4]]
+# for i in range(2):
+#     print(list[i])
+
+# # 46)
+# list = [[1, 2], [3, 4]]
+# i = -1
+# while i <= 0:
+#     i = i + 1
+#     print(list[i])
+
+# # 47)
+# for i in range(1,6):
+#     print(i)
+
+# # 48)
+# i = 0
+# while i <= 4:
+#     i = i + 1
+#     print(i)
+
+# # 49)
+# strg = "loop"
+# for i in range(0, len(strg)):
+#     print(strg[i])
+
+# # 50)
+# strg = "loop"
+# i = -1
+# while i <= len(strg)-2:
+#     i = i + 1
+#     print(strg[i])
---
+++ level 26/homework/spm.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+time.sleep(1)
+
+for _ in range(1000):
+    pyautogui.typewrite('hello')
+    pyautogui.press('enter')
+#ეს დავალებაა არაა უბრალოდ მაგარი რაღაცაა და აქ დავტოვე
