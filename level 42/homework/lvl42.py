467f3f6a243a20b8b9f0691515e8e710d12eefa5
ilia543 <mrmainxpx@gmail.com>
2024-08-15 16:23:54 +0400

    lvlsigma

---
+++ level 42/homework/lvl42.py
@@ -0,0 +1,3 @@
+# https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/python
+# https://www.codewars.com/kata/57f609022f4d534f05000024/train/python
+# https://www.codewars.com/kata/59f08f89a5e129c543000069/train/python
