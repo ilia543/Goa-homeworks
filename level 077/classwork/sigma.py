d7ae60d0844db1a947da97d3931133c41544813c
ilia543 <mrmainxpx@gmail.com>
2024-11-06 00:40:59 +0400

    commit

---
+++ level 077/classwork/sigma.py
@@ -0,0 +1,20 @@
+# arg1 = 7
+# arg2 = 14
+# arg3 = 3
+
+
+# def add(a,b):
+#     return a+b
+
+# def gamokleba(a,b):
+#     return a-b
+
+# jami = add(arg1, arg2) #21
+
+# print(gamokleba(jami, arg3))
+
+
+def kvadrati(number1):
+    print(int(number1) ** 2)
+
+kvadrati(input("number 1 = "))
